6194dadd8d13cf450b8105b9150d5eca4718f0a8
Kartika <kartikapuniyani@gmail.com>
2023-11-01 10:49:15 +0530

    refactor entity class

--- src/main/java/com/aws/practice/domain/ServiceRecord.java
+++ src/main/java/com/aws/practice/domain/ServiceRecord.java
@@ -1,7 +1,9 @@
 package com.aws.practice.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class ServiceRecord {
 
     @Id
@@ -28,54 +32,6 @@ public class ServiceRecord {
         this.name = name;
         this.type = type;
         this.jobId = jobId;
-        this.instanceId = instanceId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getJobId() {
-        return jobId;
-    }
-
-    public void setJobId(String jobId) {
-        this.jobId = jobId;
-    }
-
-    public List<String> getFiles() {
-        return files;
-    }
-
-    public void setFiles(List<String> files) {
-        this.files = files;
-    }
-
-    public String getInstanceId() {
-        return instanceId;
-    }
-
-    public void setInstanceId(String instanceId) {
         this.instanceId = instanceId;
     }
 }
